c96d91a6f7ded66b24e972b86a8e9c1145da06d5
ahmedElabd <a.alabd@qoyod.com>
2024-03-28 22:17:52 +0200

    add firebase configuration

--- wishlist-plugin.js
+++ wishlist-plugin.js
@@ -1,3 +1,4 @@
+import { initializeApp } from "firebase/app";    
     jQuery(document).ready(function($) {
     // Get the product ID from the page (you may need to modify this based on your theme structure)
     var productId = $('.product').data('product-id');
@@ -8,9 +9,13 @@
 
         // Send data to Firebase
         var firebaseConfig = {
-            apiKey: 'YOUR_API_KEY',
-            authDomain: 'YOUR_AUTH_DOMAIN',
-            databaseURL: 'YOUR_DATABASE_URL',
+            apiKey: 'AIzaSyCrNsSDB7wUlBUbHBfVkc2-YlkGeF0UAB8',
+            authDomain: 'page-builder1.firebaseapp.com',
+            // databaseURL: 'YOUR_DATABASE_URL',
+            projectId: "page-builder1",
+            storageBucket: "page-builder1.appspot.com",
+            messagingSenderId: "9136430826",
+            appId: "1:9136430826:web:7acaaa1f01eee933238193"
             // Add other Firebase config options
         };
         firebase.initializeApp(firebaseConfig);
